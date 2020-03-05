b6128ba4b487d6baf75ff40ff4547dbd8d9f275a
Skyler3259 <54539726+Skyler3259@users.noreply.github.com>
2020-03-05 08:31:54 -0600

    Add files via upload

---
+++ Toggle.js
@@ -0,0 +1,8 @@
+function Toggle() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
